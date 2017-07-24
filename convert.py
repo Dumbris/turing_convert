b82eac2ead80e8b67035c7129ab44dfabc466ab0
Algis <a.dumbris@gmail.com>
2017-07-24 21:17:30 +0300

    Add tokenizer. Add columns for alice and bob threads

--- convert.py
+++ convert.py
@@ -1,10 +1,14 @@
 import json
 import os
 import pandas as pd
+import nltk
 # Initialize global variables
 DATASETS_DIR = './'
 TEST_FILE = 'example_nolabel.json'
-END_OF_STRING = '  '
+END_OF_STRING = ' '
+TOKEN_DELIMETR = ' '
+ALICE_ID = 'Alice'
+BOB_ID = 'Bob'
 
 #debug
 import pprint
@@ -12,25 +16,45 @@ pp = pprint.PrettyPrinter(indent=4)
 
 context = []
 thread = []
+thread_alice = []
+thread_bob = []
 dialogId = []
+
+
+tknzr = nltk.word_tokenize
 
 with open(os.path.join(DATASETS_DIR, TEST_FILE), encoding='utf-8') as fh:
     data = json.load(fh)
     for row in data:
-        context.append(row['context'])
+        context.append(TOKEN_DELIMETR.join(tknzr(row['context'])))
         dialogId.append(row['dialogId'])
         utts = []
+        utts_alice = []
+        utts_bob = []
         for utt in row["thread"]:
-            utts.append(utt["text"])
-        thread.append(END_OF_STRING.join(utts))
+            tokenized_text = TOKEN_DELIMETR.join(tknzr(utt["text"]))
+            utts.append(tokenized_text)
+            if utt["userId"] == ALICE_ID:
+                utts_alice.append(tokenized_text)
+            if utt["userId"] == BOB_ID:
+                utts_bob.append(tokenized_text)
 
+        thread.append(END_OF_STRING.join(utts))
+        thread_alice.append(END_OF_STRING.join(utts_alice))
+        thread_bob.append(END_OF_STRING.join(utts_bob))
 
 #debug
 #pp.pprint(thread)
 #pp.pprint(context)
 #pp.pprint(dialogId)
 
-pd.DataFrame.from_dict({'id': dialogId, 'context': context, 'response': thread}) \
+pd.DataFrame.from_dict({'id': dialogId,
+                        'context': context,
+                        'response': thread,
+                        'response_alice': thread_alice,
+                        'response_bob': thread_alice
+                        }
+                       ) \
     .set_index('id') \
     .to_csv('test.csv')
 
